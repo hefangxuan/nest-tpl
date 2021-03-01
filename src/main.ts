f885d7376a5879c72767d601b84a8da66be6a29e
hefangxuan <hefangxuan@qq.com>
2021-03-01 16:47:02 +0800

    - 增加静态资源配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+// 创建logger日志类
+const logger = new Logger('Hefx:: bootstrap');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // 定义端口号
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get('app.APP_PORT');
+
+  app.useStaticAssets(join(__dirname, '../public'), {
+    prefix: '/static/',
+  });
+
+  await app.listen(PORT, '0.0.0.0');
+
+  logger.log(`Hefx API Server started on: ${await app.getUrl()}`);
 }
-bootstrap();
+bootstrap().then();
